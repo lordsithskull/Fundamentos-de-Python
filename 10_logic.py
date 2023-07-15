22fb04e476dfb39f14c5ce30e33a1c49abe84a62
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-15 19:28:50 +0000

    clase de opereadores lógicos

--- 10_logic.py
+++ 10_logic.py
@@ -1,2 +1,22 @@
 print('AND')
-print("True AND True => ", True)
+print("True AND True => ", True and True)
+print("True AND False => ", True and False)
+print("False AND False => ", False and False)
+print("True AND False => ", False and True)
+
+print("10 > 5 y  5< 10 ", 10 > 5 and 5 < 10)
+print("10 > 5 y  5 > 10 ", 10 > 5 and 5 > 10)
+
+stock = input('Ingresa el numero de "Stock" ')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
+
+print('OR')
+print("True OR True => ", True or True)
+print("True OR False => ", True or False)
+print("False OR False => ", False or False)
+print("True OR False => ", False or True)
+
+role = input('Digita el rol => ')
+print(role == 'admin' or role == 'seller')
