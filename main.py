df41a5be4125d126770e8ab6d9a3b5f4562d87a3
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-16 19:42:40 +0000

    Se genera juego piedra , papel o tijera v.1

--- main.py
+++ main.py
@@ -8,3 +8,27 @@ print("15/3 = ", 15/3)
 
 #Esto es un comentario
 '''
+
+user_option = input('Piedra, papel o tijeras => ')
+computer_option = 'papel'
+
+if user_option == computer_option:
+  print('Empate')
+#caso piedra
+elif user_option == 'piedra':
+  if computer_option == 'tijeras':
+    print('La piedra vence a la tijeras')
+  elif computer_option == 'papel':
+    print('El papel VENCE a la piedra')
+#caso papel
+elif user_option == 'papel':
+  if computer_option == 'tijeras':
+    print('Las tijeras VENCEN al papel')
+  elif computer_option == 'piedra':
+    print('El papel VENCE a la piedra')
+
+elif user_option == 'tijeras':
+  if computer_option == 'piedra':
+    print('La piedra VENCE a las tijeras')
+  elif computer_option == 'papel':
+    print('Las tijeras VENCEN al papel')
