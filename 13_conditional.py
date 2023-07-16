35de47b6dbde9a933fdba966e6a8ea225250975d
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-16 04:10:51 +0000

    se hace el reto par e impar

--- 13_conditional.py
+++ 13_conditional.py
@@ -1,1 +1,27 @@
-if 
+if True:
+  print('debería de ejecutarse')
+  if False:
+    print('nunca se ejecuta')
+
+print('* ' * 10)
+
+pet = input('¿Cuál es tu mascota favorita? ')
+
+if pet == 'perro':
+  print('Genial tienes buen gusto')
+elif pet == 'gato':
+  print('Espero tengas suerte') 
+elif pet == 'pez':
+  print('Eres lo máximo')
+else:
+  print ('No tienes ninguna mascota intersante')
+
+print('* ' * 10)
+'''
+stock = int(input('Digita el stock => '))
+
+if stock >= 100 and  stock <= 1000:
+  print('El stock es correcto')
+else:
+  print('El stock es incorrecto')
+'''
---
+++ reto_par-impar.py
@@ -0,0 +1,7 @@
+numero = int(input('Introduce un numero '))
+residuo = numero % 2
+
+if residuo == 0:
+  print(f'El numero {numero} es par')
+elif residuo == 1:
+  print(f'El numero {numero} es impar')
