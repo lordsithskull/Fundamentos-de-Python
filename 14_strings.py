b1478c993bc33823d3990b41e7d456de9bb89355
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-17 20:14:11 +0000

    diferentes casos de strings. Se modifica juego piedra, papel o tijera, para que sin importar si el usurario escribe en mayusculas, minusculas o ambas el juego funcione

--- 14_strings.py
+++ 14_strings.py
@@ -1,4 +1,5 @@
 text = 'Ella sabe programar en Python'
+'''
 # in dentro del arreglo permite la busqueda dentro de un string
 print('Javascript' in text)
 print('Python' in text)
@@ -6,6 +7,23 @@ if 'Python' in text:
   print('Elegiste bien!!')
 else:
   print('Tambien elegiste bien')
+  '''
 #Con size podemos saber el tamaño del string
-size = len('amor       ')
+size = len('text')
 print(size)
+
+print(text)
+print(text.upper())
+print(text.lower())
+print('Cuantas "a" hay en el texto ', text.count('a'))
+print(text.swapcase())
+print(text.startswith('Ella'))
+print(text.endswith('Rust'))
+print(text.replace('Python', 'Go'))
+
+text_2 ='este es un título'
+print(text_2.capitalize())
+print(text_2.title())
+print(text_2.isdigit())
+print("398".isdigit())
+print(text.islower())
