2718ab0bf4e6734abc006533184b808533af996d
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-17 19:54:13 +0000

    uso de in, size dentro strings

---
+++ 14_strings.py
@@ -0,0 +1,11 @@
+text = 'Ella sabe programar en Python'
+# in dentro del arreglo permite la busqueda dentro de un string
+print('Javascript' in text)
+print('Python' in text)
+if 'Python' in text:
+  print('Elegiste bien!!')
+else:
+  print('Tambien elegiste bien')
+#Con size podemos saber el tamaño del string
+size = len('amor       ')
+print(size)
