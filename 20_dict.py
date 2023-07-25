7d30301e84e6cea125e3233af118c3bc933619ae
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-25 08:21:13 +0000

    Clase 2 de dictionarios en python

---
+++ 20_dict.py
@@ -0,0 +1,12 @@
+person = {
+  'name' : 'Miguel',
+  'last_name' : 'Sánchez',
+  'langs' : ['python', 'Javascrit'],
+  'age' :  99
+}
+
+print (person)
+
+person['name'] = 'Mike'
+person['edad'] = '35'
+print(person)
