5c473b436662e2491d23221166dd9929bb403f21
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-25 07:32:06 +0000

    se agrega tupla de opciones, funcion random y un if para cuando el usuario elige una opcion invalida en el juego 'piedra', 'papel', o 'tijeras'

--- main.py
+++ main.py
@@ -8,17 +8,28 @@ print("15/3 = ", 15/3)
 
 #Esto es un comentario
 '''
+#import random funcion para usar valores aleatorios
+import random
+#tuple 
+options = ('piedra', 'papel', 'tijeras')
+
 
 user_option = input('Piedra, papel o tijeras => ')
 user_option = user_option.lower()
-computer_option = 'papel'
+computer_option = random.choice(options)
+
+if not user_option in options:
+  print('esa opcion no es valida')
+
+print ('User option => ', user_option)
+print('Computer option => ', computer_option)
 
 if user_option == computer_option:
   print('Empate')
 #caso piedra
 elif user_option == 'piedra':
   if computer_option == 'tijeras':
-    print('La piedra vence a la tijeras')
+    print('La piedra VENCE a las tijeras')
   elif computer_option == 'papel':
     print('El papel VENCE a la piedra')
 #caso papel
