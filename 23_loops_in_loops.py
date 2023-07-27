7d51332e64f51ceab47bedbb3023f8a2d95daf78
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-07-27 02:45:44 +0000

    Se completa clase loops

---
+++ 23_loops_in_loops.py
@@ -0,0 +1,11 @@
+matrix =  [[1 , 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]
+          ]
+print(matrix[0][2])
+
+for row in matrix:
+  print(row)
+  for column in matrix:
+    print (column) in matrix
+  
